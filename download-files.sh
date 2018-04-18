0159164db3979805253632ce295da9dd20c50cd7
jzonthemtn <jeff.zemerick@mtnfog.com>
2018-04-18 08:14:19 -0400

    Making directories.

--- download-files.sh
+++ download-files.sh
@@ -2,6 +2,9 @@
 
 echo "Downloading required files."
 
+mkdir -p ./idyl-ai-nlp/idyl-ai-nlp-pos-tagging/src/test/resources/models/
+mkdir -p ./idyl-ai-nlp-language-detection/idyl-ai-nlp-language-detection-opennlp/src/main/resources/
+
 # Non-Apache licensed OpenNLP models.
 wget http://opennlp.sourceforge.net/models-1.5/en-pos-maxent.bin -O ./idyl-ai-nlp/idyl-ai-nlp-pos-tagging/src/test/resources/models/en-pos-maxent.bin
 wget http://opennlp.sourceforge.net/models-1.5/en-sent.bin -O ./idyl-ai-nlp/idyl-ai-nlp-sentence-detection/src/test/resources/en-sent.bin
